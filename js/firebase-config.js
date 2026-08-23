9b6be90aeec14c2c4a0e5cbaf7ebc8c1b0f02d4e
caff2ii <caffcheung@gmail.com>
2026-08-24 00:33:49 +1000

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+// TODO: 填入你的 Firebase Config
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "XXXXX",
+    appId: "XXXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
